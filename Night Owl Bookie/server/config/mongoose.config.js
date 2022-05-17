b9afc8cdf2498d1aab903534989c026b47ed1ed5
Claude Will <claude.will@yahoo.com>
2022-05-17 16:53:16 -0500

    Making initial commit to main repositoy with server and client created so both those working on backend and frontend have something to branch and pull from

---
+++ Night Owl Bookie/server/config/mongoose.config.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const BookDb = 'bookDb';
+
+mongoose.connect(`mongodb://localhost/${BookDb}`,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(()=>console.log(`Established connection with ${BookDb}`))
+    .catch((err)=>console.log('Something went wrong connecting to the database'))
---
+++ Night Owl Bookie/server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+require('./config/mongoose.config.js');
+//require('./routes/book/routes')(app)
+
+const port = 8000;
+
+app.listen(port,()=>console.log(`Listening on port ${port}`));
