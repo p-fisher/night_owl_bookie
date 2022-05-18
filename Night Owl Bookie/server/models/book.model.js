16a98a0f1baca3251dc3156ff88f28465c5a4424
Claude Will <claude.will@yahoo.com>
2022-05-18 09:31:54 -0500

    work completed on book.model.js

---
+++ Night Owl Bookie/server/models/book.model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const BookSchema = new mongoose.Schema({
+    title:{
+        type: String,
+        required: [true,'Title is required!'],
+        minlength: [2,'Title must be at least 2 characters in length'],
+        unique: [true,'Book title must be unqiue']
+    },
+    authorName: {
+        type: String,
+        required: [true,'Author\'s name is required!'],
+        minlength: [2,'Name must be at least 2 characters in length']
+    },
+    year: { /*publication year */
+        type: Number,
+        required: [true,'Year of publication is required'],
+        minlength: [4,'Year must be 4 digits'],
+        maxlength: [4,'Year must be 4 digits']
+    },
+    pages: {
+        type: Number,
+        required: [true,'Number of pages required']
+    },
+    likes: {
+        type: Number
+    }
+
+},{timestamps:true})
+
+const Book = mongoose.model("Book",BookSchema);
+module.exports = Book;
