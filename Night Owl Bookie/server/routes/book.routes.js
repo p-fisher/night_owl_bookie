054fe250d217d2371d67d012bc36d6e3ce1f0549
p-fisher <ops.patrickfisher@gmail.com>
2022-05-24 14:20:58 -0400

    backend tidying PF MA

--- Night Owl Bookie/server/routes/book.routes.js
+++ Night Owl Bookie/server/routes/book.routes.js
@@ -5,7 +5,6 @@ module.exports = (app) => {
     app.post("/api/books", bookCtrl.createNewBook);
     // routes that require an ID should come after those that don't
     app.get("/api/books/:id", bookCtrl.findOneBook);
-    
     app.put("/api/books/:id", bookCtrl.updateExistingBook);
     app.delete("/api/books/:id", bookCtrl.deleteAnExistingBook);
 };
